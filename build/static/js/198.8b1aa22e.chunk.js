303a4239a8f4e70bd7c506fde26990d096d68552
Leandro Roland <84057445+leandroroland@users.noreply.github.com>
2024-08-21 15:18:51 -0300

    Add files via upload

---
+++ build/static/js/198.8b1aa22e.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkfinanzas_app=self.webpackChunkfinanzas_app||[]).push([[198],{198:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=198.8b1aa22e.chunk.js.map
